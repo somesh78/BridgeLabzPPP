8b7ad69c6e2eb7d10561ab4dc06a7a62a4d947d8
somesh78 <rajputsomesh78@gmail.com>
2025-09-05 01:21:52 +0530

    Programming_Elements questions completed

---
+++ Programming_Elements/Level1/KmToMiles.java
@@ -0,0 +1,10 @@
+package Programming_Elements.Level1;
+
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km / 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
